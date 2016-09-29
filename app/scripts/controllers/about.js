0cc86721be0b4f3c705da8a4f5be8eb9a2847ec3
stephen <rust.stephen@gmail.com>
2016-09-29 17:43:15 -0500

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name srustmeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the srustmeApp
+ */
+angular.module('srustmeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name srustmeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the srustmeApp
+ */
+angular.module('srustmeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
